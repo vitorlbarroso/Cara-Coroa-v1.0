a0450bde8d32fc5b3d27aaffc6d2277e8e914ec5
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2021-08-19 18:22:07 -0300

    Add files via upload

---
+++ Halzz_CaraCoroa/client-side/client.lua
@@ -0,0 +1,16 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+
+--------------------------------------------------------------------------------
+-- Configurações:
+--------------------------------------------------------------------------------
+
+requestmensageminiciado = true -- true: Mostrar mensagem quando iniciar / false: Não mostrar mensagem quando iniciar
+
+--------------------------------------------------------------------------------
+-- Código:
+--------------------------------------------------------------------------------
+
+if requestmensageminiciado then
+    print("Script iniciado com sucesso!")
+end
---
+++ Halzz_CaraCoroa/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'HalZz#0001'
+description 'Script que sorteia cara ou coroa'
+version '1.0.0'
+
+
+client_scripts {
+    '@vrp/lib/utils.lua',
+    'client-side/*.lua',
+    }
+
+server_script {
+    '@vrp/lib/utils.lua',
+    'server-side/*.lua',
+    }
