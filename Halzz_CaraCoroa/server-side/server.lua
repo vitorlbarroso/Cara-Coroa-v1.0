871d17a8d70b5b848b4d5e86b419642f24f9a91f
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2021-08-19 23:22:28 -0300

    Update server.lua

--- Halzz_CaraCoroa/server-side/server.lua
+++ Halzz_CaraCoroa/server-side/server.lua
@@ -7,7 +7,7 @@ local Proxy = module("vrp","lib/Proxy")
 
 print("Verificando o Script, aguarde!")
 
-comandoscript = 'caracoroa' -- Comando que será utilizado para jogar.
+local comandoscript = 'caracoroa' -- Comando que será utilizado para jogar.
 
 --------------------------------------------------------------------------------
 -- Código:
@@ -15,12 +15,11 @@ comandoscript = 'caracoroa' -- Comando que será utilizado para jogar.
 
 RegisterCommand(comandoscript, function(source,args,rawCommand) -- Registrando o comando.
     if source > 0 then -- Definindo se a Source do Player é maior que a do Sistema.
-        function sorteiocaracoroa() -- Iniciando a função de sorteio dos dados.
-            local tabelacaracoroa = {"Cara","Coroa"} -- Tabela com os dados "Cara" e "Coroa".
-            for k,v in pairs(tabelacaracoroa) do -- Laço para verificar os dados que contém na tabela.
-                return tabelacaracoroa[math.random(#tabelacaracoroa)] -- Adentrando a tabela e criando uma math.random de acordo com a largura da tabela.
-            end -- Finaliza o For.
-        end -- Finaliza a Função.       
         TriggerClientEvent('chatMessage',-1,'A moeda girou e caiu:  '..(sorteiocaracoroa()))
     end -- Finaliza a Source.
 end)
+
+function sorteiocaracoroa() -- Iniciando a função de sorteio dos dados.
+    local tabelacaracoroa = {"Cara","Coroa"} -- Tabela com os dados "Cara" e "Coroa".
+    return tabelacaracoroa[math.random(#tabelacaracoroa)] -- Adentrando a tabela e criando uma math.random de acordo com a largura da tabela.
+end -- Finaliza a Função.       
